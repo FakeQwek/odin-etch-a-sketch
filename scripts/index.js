8ea49477cd3fc2cc0ffadce1aafb4d3cdc738788
FakeQwek <S10255665@connect.np.edu.sg>
2025-03-13 17:37:46 +0800

    feat: Add functionality to sketchboard

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 let sketchBoard = document.getElementById("sketch-container");
+const colours = ["87A878", "B0BC98", "D9DAD5", "CAE2BC", "DBF9B8"];
 
 for(let i = 0; i < 16; i++) {
-    console.log("run");
+
     sketchBoard.insertAdjacentHTML("beforeend", `<div class="sketch-col col-${i}"></div>`);
     for (let x = 0; x < 16; x++) {
         let currentCol = document.querySelector(`.col-${i}`);
@@ -9,6 +10,36 @@ for(let i = 0; i < 16; i++) {
             `
                 <div class="sketch-cell"></div>
             `);
-    }
-   
+    } 
 }
+
+let cellElements = sketchBoard.getElementsByClassName("sketch-cell");
+for(let i = 0 ; i < cellElements.length; i++) {
+    cellElements[i].addEventListener("mouseenter", () => {
+   
+        let currentCell = event.target;
+        let randNo = Math.floor(Math.random() * 5);
+
+        if(currentCell.style.backgroundColor == "") {
+            currentCell.style.backgroundColor = "#" + colours[randNo];  
+        }
+
+        if(currentCell.style.opacity == "") {
+            currentCell.style.opacity = "10%";
+        }
+        else if (currentCell.style.opacity != 1) {
+            let currentOpacity = Number(currentCell.style.opacity);
+            currentCell.style.opacity = currentOpacity + 0.1;
+        }
+
+    });
+};
+
+
+/*
+sketchBoard.addEventListener("mouseenter", () => {
+    console.log("hovering");
+    let currentCell = event.target;
+    currentCell.style.backgroundColor = "black";
+});
+*/
